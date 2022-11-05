7c27e55638f44d5abc239fcac96c200e3e39fa25
Davi Coelho <davidiascoelho22@gmail.com>
2022-11-05 15:59:25 -0300

    Página detalhe produto pronta

--- src/screens/DetalheProduto/index.jsx
+++ src/screens/DetalheProduto/index.jsx
@@ -1,8 +1,9 @@
-import { Card } from "react-bootstrap"
-import { Cards } from "../../components/Cards/Cards"
+import { Button, Card, Col, Row } from "react-bootstrap"
 import { api } from '../../api';
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { BodyContainer } from '../../components/BodyContainer/BodyContainer'
+import './DetalheProduto.css'
 
 export const DetalheProduto = () => {
     const {id} = useParams();
@@ -18,8 +19,32 @@ export const DetalheProduto = () => {
       });
   }, []);
 
-
     return (
-        <h1>asidjapsdjasp</h1>
+        <BodyContainer>
+            <Row>
+                <Col>
+                    <div className="esquerda">
+                        <img src={produto.fotoLink} />
+                    </div>
+                </Col>
+
+                <Col>
+                    <div className="direita">
+                        <h1>{produto.nome}</h1>
+                        <p>{produto.descricao}</p>
+                        <i className="bi bi-star-fill"></i>
+                        <i className="bi bi-star-fill"></i>
+                        <i className="bi bi-star-fill"></i>
+                        <i className="bi bi-star-fill"></i>
+                        <i className="bi bi-star-fill"></i>
+                        
+                        <div className="botao">
+                            <Button variant="success" size="lg">Comprar <i className="bi bi-bag-check"></i></Button>{' '}
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+        </BodyContainer>
+        
     )
 }
