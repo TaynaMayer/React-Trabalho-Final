492bc3318970579b520d16afc68c86303f401af9
Tayná Mayer <112437100+TaynaMayer@users.noreply.github.com>
2022-11-06 13:39:08 -0300

    Atualização Home

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -24,24 +24,31 @@ export const Cards = () => {
             return(
               <Col md="6" sm= "12" lg= "4" xl= "4">                              
                 <Card style={{ 
-                  height: '525px',
+                  height: '400px',
                   width: '250px',                 
                   border: '1px solid',
-                  marginTop: '70px',
+                  marginTop: '80px',
+                  marginBottom: '30px',
                   textAlign: 'center'
-                  
-                            
+                                       
                   
               }}>
               
-                <Card.Img variant="top" src={produto.fotoLink} />
+              <Card.Img 
+                style={{ 
+                  height: '190px',
+                  width: '248px',                                 
+                  
+                  }}
+                variant="top" 
+                src={produto.fotoLink} />
                 <Card.Body>
                   
                   <Card.Title>{produto.nome}</Card.Title>
                   <Card.Text>{produto.descricao}</Card.Text>
                   <Card.Text>R${produto.valor}</Card.Text>
-                  <Button variant="success"><Link to={`/DetalheProduto/${produto.id}`}>Comprar</Link></Button>{' '}
-                
+                  <Button variant="success"><Link style={{color:'#FFF'}} to={`/DetalheProduto/${produto.id}`}>Comprar</Link></Button>{' '}
+                  
                 </Card.Body>
                 
               </Card>
