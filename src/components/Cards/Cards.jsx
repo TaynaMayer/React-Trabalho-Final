4da9e5a65908433b02c07b03e2cca65d923467fb
Davi Coelho <davidiascoelho22@gmail.com>
2022-11-07 19:38:54 -0300

    Fazendo styled components

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -5,8 +5,9 @@ import { api } from '../../api';
 import React, { useEffect, useState } from "react";
 import { Col } from 'react-bootstrap';
 import './Style.css';
+import { StyleCard } from './Style';
 
-export const Cards = () => {
+export const Cards = (props) => {
   const [produtos, setProduto] = useState([]);
   useEffect(() => {
     api
@@ -18,45 +19,22 @@ export const Cards = () => {
   }, []);
 
     return (
-
           <>
           {produtos.map((produto) => {
             return(
-              <Col md="6" sm= "12" lg= "4" xl= "4">                              
-                <Card style={{ 
-                  height: '400px',
-                  width: '250px',                 
-                  border: '1px solid',
-                  marginTop: '80px',
-                  marginBottom: '30px',
-                  textAlign: 'center'
-                                       
-                  
-              }}>
-              
-              <Card.Img 
-                style={{ 
-                  height: '190px',
-                  width: '248px',                                 
-                  
-                  }}
-                variant="top" 
-                src={produto.fotoLink} />
+              <Col key={produto.id} md="6" sm= "12" lg= "4" xl= "4">                              
+              <StyleCard height='500px' width='290px' margintop='80px' marginbottom='30px' textalign='center'>
+              <Card.Img variant="top" src={produto.fotoLink} />
                 <Card.Body>
-                  
                   <Card.Title>{produto.nome}</Card.Title>
                   <Card.Text>{produto.descricao}</Card.Text>
                   <Card.Text>R${produto.valor}</Card.Text>
                   <Button variant="success"><Link style={{color:'#FFF'}} to={`/DetalheProduto/${produto.id}`}>Comprar</Link></Button>{' '}
-                  
                 </Card.Body>
-                
-              </Card>
+              </StyleCard>
               </Col>
             )
       } ) }
-        </>   
+          </>   
     );
-
-
 }
--- src/screens/Home/Index.jsx
+++ src/screens/Home/Index.jsx
@@ -12,7 +12,6 @@ export const Home = () => {
                 <Row>
                     <Cards />
                 </Row>
-                
             </BodyContainer>
         </>
     );
