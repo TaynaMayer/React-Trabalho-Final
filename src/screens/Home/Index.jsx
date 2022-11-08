ffac1c4a0038893fd7ad17325bab16eb5d9fcfca
Rafael Gomez <rafaelgomez@MacBook-Pro-de-Rafael.local>
2022-11-07 22:30:50 -0300

    Banner black

--- src/screens/Home/Index.jsx
+++ src/screens/Home/Index.jsx
@@ -3,11 +3,20 @@ import { BodyContainer } from "../../components/BodyContainer/BodyContainer";
 import { Cards } from "../../components/Cards/Cards";
 import { Footer } from "../../components/Footer/Footer";
 import { Header } from "../../components/Header/Header"
+import banner from "../../assets/banner.png"
 import './Home.css';
 
 export const Home = () => {
     return (
         <>
+        <div>
+        <img style={{
+            resizeMode: "auto",
+            height: 600,
+            width: '100%'
+          }} 
+        className="banner" src={banner} alt="MARIO"  />
+        </div>
             <BodyContainer>
                 <Row>
                     <Cards />
